bfe1ffbde35cf344eb3661574e19bc98f5c302fb
__ALASTOR__ <103386430+MihailHardikov@users.noreply.github.com>
2024-07-12 15:18:44 +0300

    8

--- server.mjs
+++ server.mjs
@@ -9,7 +9,7 @@ createServer((req, res) => {
 
 
     let responseMessage = "Hello " + name + "! " + message + "!";
-    responseMessage.replace(/['"]/g, '');
+    responseMessage = responseMessage.replace(/['"]/g, '');
 
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
     res.write(responseMessage);
